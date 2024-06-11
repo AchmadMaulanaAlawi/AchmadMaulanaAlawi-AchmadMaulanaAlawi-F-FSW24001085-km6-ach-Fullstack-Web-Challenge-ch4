435d58290e2f86467f9e7741ed3329be1d03ed1e
Achmad Maulana Alawi <alawitch.b@gmail.com>
2024-06-11 19:04:53 +0700

    set admin role when register with google

--- usecase/auth/index.js
+++ usecase/auth/index.js
@@ -1,75 +1,75 @@
-const jsonwebtoken = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 const {
   createUser,
   getUserByEmail,
   getUserByID,
   getGoogleAccessTokenData,
-} = require("../../repository/user");
+} = require("../../repository/user")
 
-const { createToken } = require("./util");
+const { createToken } = require("./util")
 
 exports.register = async (payload) => {
-  let user = await createUser(payload);
+  let user = await createUser(payload)
 
   // Delete object password from user
-  delete user?.dataValues?.password;
+  delete user?.dataValues?.password
 
   // Create token
-  const data = createToken(user);
+  const data = createToken(user)
 
-  return data;
-};
+  return data
+}
 
 exports.login = async (email, password) => {
   // get the user
-  let user = await getUserByEmail(email, true);
+  let user = await getUserByEmail(email, true)
   if (!user) {
-    throw new Error(`User is not found!`);
+    throw new Error(`User is not found!`)
   }
 
   // compare the password
-  const isValid = await bcrypt.compare(password, user?.password);
+  const isValid = await bcrypt.compare(password, user?.password)
   if (!isValid) {
-    throw new Error(`Wrong password!`);
+    throw new Error(`Wrong password!`)
   }
 
   // delete password
   if (user?.dataValues?.password) {
-    delete user?.dataValues?.password;
+    delete user?.dataValues?.password
   } else {
-    delete user?.password;
+    delete user?.password
   }
 
   // Create token
-  const data = createToken(user);
+  const data = createToken(user)
 
-  return data;
-};
+  return data
+}
 
 exports.profile = async (id) => {
   // get the user
-  let data = await getUserByID(id);
+  let data = await getUserByID(id)
   if (!data) {
-    throw new Error(`User is not found!`);
+    throw new Error(`User is not found!`)
   }
 
   // delete password
   if (data?.dataValues?.password) {
-    delete data?.dataValues?.password;
+    delete data?.dataValues?.password
   } else {
-    delete data?.password;
+    delete data?.password
   }
 
-  return data;
-};
+  return data
+}
 
 exports.googleLogin = async (accessToken) => {
   // validate the token and get the data from google
-  const googleData = await getGoogleAccessTokenData(accessToken);
+  const googleData = await getGoogleAccessTokenData(accessToken)
 
   // get is there any existing user with the email
-  let user = await getUserByEmail(googleData?.email);
+  let user = await getUserByEmail(googleData?.email)
 
   // if not found
   if (!user) {
@@ -79,14 +79,15 @@ exports.googleLogin = async (accessToken) => {
       password: "",
       name: googleData?.name,
       picture: googleData?.picture,
-    });
+      role: "admin",
+    })
   }
 
   // Delete object password from user
-  delete user?.dataValues?.password;
+  delete user?.dataValues?.password
 
   // create token
-  const data = createToken(user);
+  const data = createToken(user)
 
-  return data;
-};
+  return data
+}
