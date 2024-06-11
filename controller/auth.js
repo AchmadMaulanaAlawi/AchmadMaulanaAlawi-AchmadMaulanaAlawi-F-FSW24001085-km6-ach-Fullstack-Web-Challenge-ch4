2f472bbf773c3694ef9c2e67e5c0d18eeb4d91c2
Achmad Maulana Alawi <alawitch.b@gmail.com>
2024-06-11 16:55:53 +0700

    set role to admin

--- controller/auth.js
+++ controller/auth.js
@@ -1,30 +1,30 @@
-const { register, login, googleLogin } = require("../usecase/auth");
+const { register, login, googleLogin } = require("../usecase/auth")
 
 exports.register = async (req, res, next) => {
   try {
     // get the body
-    const { email, password, name } = req?.body;
+    const { email, password, name } = req?.body
 
     // get the photo
-    const photo = req?.files?.photo;
+    const photo = req?.files?.photo
 
     if (email == "" || !email) {
       return next({
         message: "Email must be filled!",
         statusCode: 400,
-      });
+      })
     }
     if (password == "" || !password) {
       return next({
         message: "Password must be filled!",
         statusCode: 400,
-      });
+      })
     }
     if (name == "" || !name) {
       return next({
         message: "Name must be filled!",
         statusCode: 400,
-      });
+      })
     }
 
     const data = await register({
@@ -32,80 +32,81 @@ exports.register = async (req, res, next) => {
       password,
       name,
       photo,
-    });
+      role: "admin",
+    })
 
     res.status(200).json({
       message: "Success",
       data,
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 exports.login = async (req, res, next) => {
   try {
     // get the body
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
     if (email == "" || !email) {
       return next({
         message: "Email must be filled!",
         statusCode: 400,
-      });
+      })
     }
     if (password == "" || !password) {
       return next({
         message: "Password must be filled!",
         statusCode: 400,
-      });
+      })
     }
 
-    const data = await login(email, password);
+    const data = await login(email, password)
 
     res.status(200).json({
       message: "Login success",
       data,
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 exports.googleLogin = async (req, res, next) => {
   try {
     // get the body
-    const { access_token } = req.body;
+    const { access_token } = req.body
 
     if (!access_token) {
       return next({
         statusCode: 400,
         message: "Access token must be provided!",
-      });
+      })
     }
 
     // login with google logic
-    const data = await googleLogin(access_token);
+    const data = await googleLogin(access_token)
 
     res.status(200).json({
       message: "Success",
       data,
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 exports.profile = async (req, res, next) => {
   try {
     // get user by id
-    const data = req.user;
+    const data = req.user
 
     res.status(200).json({
       message: "Success",
       data,
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
